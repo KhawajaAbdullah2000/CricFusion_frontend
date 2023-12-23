3b7ffe60ce08ae09ce83a931af43b6e0805bf427
k200385 <k200385@nu.edu.pk>
2023-12-23 14:33:59 +0500

    done till schedule

--- assets/components/orgaization/MatchesSchedules.js
+++ assets/components/orgaization/MatchesSchedules.js
@@ -1,8 +1,9 @@
-import { View, Text,StyleSheet,FlatList,TouchableOpacity,Image,Modal,Pressable } from 'react-native'
+import { View, Text,StyleSheet,FlatList,Modal,Pressable } from 'react-native'
 import React,{useState,useEffect} from 'react'
 import client from '../../api/client';
 import { useLogin } from '../../context/LoginProvider';
 import Apploader from "../Apploader";
+import { useFocusEffect } from '@react-navigation/native';
 
 
 const MatchesSchedules = () => {
@@ -40,33 +41,50 @@ const MatchesSchedules = () => {
   }
 
 
-  useEffect(()=>{
-    console.log("At schedule screen");
-    fetchLeagueDetails(contextLeague_id);
-    fetchLeagueSchedule(contextLeague_id);
+  // useEffect(()=>{
+  //   console.log("At schedule screen");
+  //   fetchLeagueDetails(contextLeague_id);
+  //   fetchLeagueSchedule(contextLeague_id);
    
-  },[])
+  // },[])
+
+  useFocusEffect(
+    React.useCallback(() => {
+      console.log("at fcus effect");
+      // This function will be called every time the screen comes into focus
+      fetchLeagueDetails(contextLeague_id);
+      fetchLeagueSchedule(contextLeague_id);
+    }, [contextLeague_id])
+  );
 
   
   const renderItem = ({ item }) => (
 
-    <View style={{marginTop:20,marginLeft:10}}>
+    <View>
     
-    <Text>Team1: {item.Team1.name}</Text>
-    <Text>Team1: {item.Team2.name}</Text>
-    <Text>{new Date(item.match_date).toLocaleDateString('en-us',{
-      'year':'2-digit',
-      'month':'short',
-      'day':'2-digit'
-    })}</Text>
+<Pressable style={[styles.row]} onLongPress={()=>console.log(item.venue)}>
+     <Text style={styles.col}>{new Date(item.match_date).toLocaleDateString('en-us',{
+      'day':'2-digit',
+      'month':'short'
+    
+     })} {new Date(item.match_date).toLocaleString('en-us',{
+      
+        'hour':'numeric',
+       'minute':'numeric',
+       'timeZone':'Asia/Karachi'
+       })}
 
-    <Text>{new Date(item.match_date).toLocaleString('en-us',{
-     'hour':'numeric',
-     'minute':'numeric',
-     'timeZone':'Asia/Karachi'
-    })}</Text>
-    
+     
+     </Text>
+      <View style={styles.centeredCol}>
+        <Text>{item.Team1.name} vs {item.Team2.name}</Text>
+      </View>
+      <Text style={styles.col}>{item.venue}</Text>
+      </Pressable>
     </View>
+
+   
+
     
 
 
@@ -78,6 +96,7 @@ const MatchesSchedules = () => {
     <View style={styles.container}>
       <Text style={{fontSize:25,fontWeight:'bold',textAlign:'center',marginTop:10}}>League Schedule</Text>
 
+
       {
 
         league.length? (  
@@ -86,7 +105,9 @@ const MatchesSchedules = () => {
           </View>
        ):null
        }
+       <Text style={{fontSize:25,fontWeight:'bold',textAlign:'center',marginTop:6,marginBottom:30}}>Group Stage</Text>
 
+    
 
     {
   
@@ -99,6 +120,8 @@ const MatchesSchedules = () => {
        />
       )
       }  
+
+     
 
 
 
@@ -116,8 +139,31 @@ const MatchesSchedules = () => {
 
 const styles=StyleSheet.create({
   container:{
-    flex:1
+    flex:1,
+    padding:16,
+    backgroundColor:'lightblue'
   
+  
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1, // Add a border bottom for row separation
+    borderBottomColor: 'grey', // Border color
+    paddingVertical: 8
+  },
+  header: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  col: {
+    flex: 1, // Equal width for each column
+    textAlign: 'center',
+  },
+  centeredCol: {
+    flex: 1,
+    justifyContent: 'center', // Center the content vertically
+    textAlign: 'center',
   }
 })
 export default MatchesSchedules
